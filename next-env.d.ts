b34e992b2d87721454c117ff232fdc8728f9d59b
Sergey Chernov <chernov.sergey@gmail.com>
2025-06-05 14:18:56 +0300

    Move Next.js app to repo root

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,6 +2,4 @@
 /// <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
-
-interface Window {}
+// see https://nextjs.org/docs/pages/api-reference/config/typescript for more information.
